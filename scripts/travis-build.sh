a32d195e40351361bb6de0dad8b6ba9b3f547c82
ProTip <greg.zapp@gmail.com>
2018-08-30 16:53:01 -0500

    Fix pulling by build number

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -6,7 +6,7 @@ set -xeuo pipefail
 
 shopt -s globstar
 
-
+UPSTREAM_TAG=${UPSTREAM_TAG:-}
 UPSTREAM_ARTIFACT_BASE=${UPSTREAM_ARTIFACT_BASE:-s3://rundeck-travis-artifacts/oss}
 UPSTREAM_PROJECT=${UPSTREAM_PROJECT:-rundeck}
 
