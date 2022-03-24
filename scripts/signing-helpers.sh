a871878ff5bd4bdc298fc34ca8bff2c9eb8d85e1
chrismcg14 <chrismcg@rundeck.com>
2022-03-24 14:01:07 -0600

    remove unused arguments from span expect

--- scripts/signing-helpers.sh
+++ scripts/signing-helpers.sh
@@ -76,11 +76,11 @@ sign_rpms(){
     done
 
 
-    expect - -- $GPG_PATH $KEYID $PASSWORD  <<END
+    expect - -- $GPG_PATH $PASSWORD  <<END
       spawn gpg --import [lindex \$argv 0]/secring.gpg
         expect {
             # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
-            "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 2]\r"; log_user 1; exp_continue }
+            "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1; exp_continue }
             eof { catch wait rc; exit [lindex \$rc 3]; }
             timeout { puts "Timed out!"; exit 1 }
         }
@@ -143,11 +143,11 @@ sign_debs(){
         echo "$PREDEBSHA for artifact: $DEB"
     done
 
-    expect - -- $GPG_PATH $KEYID $PASSWORD  <<END
+    expect - -- $GPG_PATH $PASSWORD  <<END
   spawn gpg --import [lindex \$argv 0]/secring.gpg
     expect {
         # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
-        "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 2]\r"; log_user 1; exp_continue }
+        "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1; exp_continue }
         eof { catch wait rc; exit [lindex \$rc 3]; }
         timeout { puts "Timed out!"; exit 1 }
     }
@@ -156,11 +156,11 @@ sign_debs(){
     GPG_TTY=$(tty)
     export GPG_TTY
 
-    expect - -- $GPG_PATH $KEYID $PASSWORD  <<END
+    expect - -- $GPG_PATH $PASSWORD  <<END
 spawn dpkg-sig --gpg-options "-u [lindex \$argv 1]" --sign builder $DEBS
 expect {
     # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
-    "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 2]\r"; log_user 1; exp_continue }
+    "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1; exp_continue }
     eof { catch wait rc; exit [lindex \$rc 3]; }
     timeout { puts "Timed out!"; exit 1 }
 }
