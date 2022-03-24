0711b5146d5b6c801841f8bd1d0a9a4fea2f856e
chrismcg14 <chrismcg@rundeck.com>
2022-03-24 14:16:58 -0600

    remove exp continue

--- scripts/signing-helpers.sh
+++ scripts/signing-helpers.sh
@@ -80,7 +80,7 @@ sign_rpms(){
       spawn gpg --import [lindex \$argv 0]/secring.gpg
         expect {
             # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
-            "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1; exp_continue }
+            "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1}
             eof { catch wait rc; exit [lindex \$rc 3]; }
             timeout { puts "Timed out!"; exit 1 }
         }
