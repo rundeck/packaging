ca768a57ac5e3a2816d6dffdc6fdec18c2f446f1
chrismcg14 <chrismcg@rundeck.com>
2022-03-24 15:14:36 -0600

    update rpm expect, deb expect params

--- scripts/signing-helpers.sh
+++ scripts/signing-helpers.sh
@@ -76,15 +76,15 @@ sign_rpms(){
     done
 
 
-#expect - -- $GPG_PATH $PASSWORD  <<END
-#spawn gpg --import [lindex \$argv 0]/secring.gpg
-#expect {
-#    # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
-#    "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1}
-#    eof { catch wait rc; exit [lindex \$rc 3]; }
-#    timeout { puts "Timed out!"; exit 1 }
-#    }
-#END
+expect - -- $GPG_PATH $PASSWORD  <<END
+spawn gpg --import [lindex \$argv 0]/secring.gpg
+expect {
+    # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
+    "Enter passphrase:" { log_user 0; send -- "[lindex \$argv 1]\r"; log_user 1; exp_continue }
+    eof { catch wait rc; exit [lindex \$rc 3]; }
+    timeout { puts "Timed out!"; exit 1 }
+}
+END
 
     echo "=======Post import RPM======="
     
@@ -158,7 +158,7 @@ END
     GPG_TTY=$(tty)
     export GPG_TTY
 
-    expect - -- $GPG_PATH $PASSWORD  <<END
+    expect - -- $KEYID $PASSWORD  <<END
 spawn dpkg-sig --gpg-options "-u [lindex \$argv 1]" --sign builder $DEBS
 expect {
     # Passphrase prompt arrives for each deb signed; exp_continue allows this block to execute multiple times
