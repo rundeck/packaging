92e84e3de9172f35c987d51158cdb4fa93d56560
chrismcg14 <chrismcg@rundeck.com>
2022-03-25 13:30:38 -0600

    move spawn in quotes

--- scripts/signing-expect.sh
+++ scripts/signing-expect.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
 import_gpg(){
-  spawn gpg --import "$GPG_PATH"/secring.gpg
+  spawn "gpg --import "$GPG_PATH"/secring.gpg"
   expect "Please enter the passphrase to import the OpenPGP secret key:"
   send "$PASSWORD"
 }
