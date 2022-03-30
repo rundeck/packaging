4ec148802a2af216daa6208e32571436d34c6455
chrismcg14 <chrismcg@rundeck.com>
2022-03-30 15:29:49 -0600

    update max attempts

--- scripts/signing-expect.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-
-import_gpg(){
-  spawn "gpg --import "$GPG_PATH"/secring.gpg"
-  expect "Please enter the passphrase to import the OpenPGP secret key:"
-  send "$PASSWORD"
-}
-
-export -f import_gpg
